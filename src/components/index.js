e79d7e0052d06454996c072ba1d8297a3a0b9ab9
Jake Wiesler <jakewiesler@gmail.com>
2018-06-16 20:04:53 -0400

    moar and moar and moar

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Page } from './Page/Page'
 export { default as HomeSplash } from './HomeSplash'
 export { default as Prints } from './Prints'
-export { default as Thumbnail } from './Thumbnail'
