dc1e917b8a21779613236198c122d69d2ac46f97
Jake Wiesler <jakewiesler@gmail.com>
2018-08-15 22:44:13 -0400

    Style updates

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 import Thumbnail from './Thumbnail/Thumbnail'
 
 const PortfolioItem = ({ ...item }) => (
-  <Link to={item.fields.slug}>
+  <Link to={item.fields.slug} style={{ textDecoration: 'none' }}>
     <Thumbnail
       img={item.frontmatter.thumbnail.childImageSharp.sizes}
       title={item.frontmatter.title}
--- src/components/Specs/Specs.js
+++ src/components/Specs/Specs.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { StyledSpecs, Spec, Key, Value } from './styled'
 
+const keys = {
+  sqft: 'sq. ft.',
+}
+
 const Specs = ({ data }) => (
   <StyledSpecs>
     {Object.keys(data).map((spec, i) => (
       <Spec key={i}>
-        <Key>{spec}</Key>
+        <Key>{keys[spec] ? keys[spec] : spec}</Key>
         <Value>{data[spec]}</Value>
       </Spec>
     ))}
